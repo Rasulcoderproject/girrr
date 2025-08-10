c0b9230d1393029a0928d12200fcca64c9deafcc
Расул Палванов <palvanovrasulbek2299@gmail.com>
2025-08-10 17:16:33 +0300

    reffаааааааfffffааааааааffc

--- api/telegram.js
+++ api/telegram.js
@@ -238,8 +238,8 @@ async function processGameLogic(chat_id, text) {
   
   // /start
   if (text === "/start") {
-    sessions[chat_id] = {};
-    await sendMessage(chat_id, `👋 Привет, ${username }! Выбери тему для теста или игру:`, {
+    const { firstName} = sessions[chat_id] || {};
+    await sendMessage(chat_id, `👋 Привет, ${firstName}! Выбери тему для теста или игру:`, {
       keyboard: [
         [{ text: "История" }, { text: "Математика" }],
         [{ text: "Английский" }, { text: "Игры 🎲" }],
